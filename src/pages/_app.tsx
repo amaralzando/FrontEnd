3280f707f4db4cb05eeabb405b6dbcb811c8296e
Gabriel Andrade <gaamaralpessoal@gmail.com>
2024-05-10 15:39:26 -0300

    Update dependencies, add Sass, and modify commitlint configuration
    * Upgrade Next.js to 14.2.3 and add Sass as a devDependency
    * Modify commitlint configuration to use CommonJS syntax
    *

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
+import NavBar from '@/containers/navbar/Index';
+import '@/styles/globals.scss'
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NavBar />
+      <div className='dark'>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
